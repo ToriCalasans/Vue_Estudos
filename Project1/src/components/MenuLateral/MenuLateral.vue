f596f5885b9cb92acc98ec6d052d64091ed9f202
ToriCalasans <calasanstori@gmail.com>
2022-04-12 17:17:59 -0300

    commit inicial

---
+++ Project1/src/components/MenuLateral/MenuLateral.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref} from 'vue'
+
+defineProps({
+   // titulo: String
+})
+//import Rotas from 'src\components\Shared\Links\Link.vue'
+</script>
+
+<template>
+<div id="menu" >
+    <ul id="Rotas">
+		<li v-for="link in links">
+        
+            <a >{{ titulo }}</a>
+        </li>
+	</ul>
+</div>
+</template>
+
+<style scoped>
+#menu{
+   width: 30%;
+   margin: 0; padding: 0;
+   float: left;
+   background-color: #ffe4b5;
+}
+</style>
---
+++ Project1/src/components/Shared/Links/Link.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+ 
+})
+
+const Rotas ={
+    data(){
+        return{
+            links:[
+                {titulo:"Página 1", endereco:"google.com.br"},
+                {titulo:"Página 2", endereco:"google.com.br"}
+                ]
+        }
+    }
+}
+
+Vue.createApp(Rotas).mount('#Rotas')
+</script>
